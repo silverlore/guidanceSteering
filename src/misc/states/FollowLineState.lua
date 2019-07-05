22a6f6bfa78ba54f7835cc41f6f6a54488390212
Jimmy Nielsen <silverlore@silverdragons.dk>
2019-07-05 21:13:20 +0200

    fix error

--- src/misc/states/FollowLineState.lua
+++ src/misc/states/FollowLineState.lua
@@ -88,7 +88,7 @@ function FollowLineState:detectedHeadland(lastSpeed)
     local x, y, z = unpack(data.driveTarget)
 
     local speedMultiplier = (lastSpeed / 100) * 30 -- increase break distance
-    local distanceToAct = data.headheadlineArcDistance * speedMultiplier
+    local distanceToAct = data.headlineArcDistance * speedMultiplier
     local lookAheadStepDistance = distanceToAct + 5 -- m
     local distanceToHeadLand, isDistanceOnField = HeadlandUtil.getDistanceToHeadLand(self, self.object, x, y, z, lookAheadStepDistance)
 
